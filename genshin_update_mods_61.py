14e2a933f0dfbb893b747c4f84284898d22ef457
ronstec@web.de <ronstec@web.de>
2025-11-18 22:38:33 +0100

    Added existing code

---
+++ genshin_update_mods_61.py
@@ -0,0 +1,344 @@
+# Made by ronson83
+# Version 0.1
+# Thanks to the Genshin Impact Modding Community for their help and support
+
+# Fixes for Version 6.1
+
+# ############################################################################################################################
+#                                                              |c                                                             
+#                                                             @                                                               
+#                                                            @    @@@       @                                                 
+#                                                            @  @@            "                                               
+#                                                            @  @                                                             
+#                                                            @      -@@@@@@@@@@@@@                                            
+#                                                              @@@@@@aOddMJaOdOdd@@@@@                                        
+#                                                            @@@Jd@OMMdOOOOOJMOdOOOda@@@                                      
+#                                                         I@@dMdOOOOOOO@c@OOOaOMOOdddOO@@@                                    
+#                                                       @a@JdddOOOOOOOOdOOJ@cOca@JOOdOddd@@                                   
+#                                                     @ @dMOacddMdddadOdOddJa@JJ@aOOdOddddM@                                  
+#                                                      @@c@OOdOacOJJOddaJaJOOJacJ@JJaddOdcd@@                                 
+#                                                    - @@@J@JdOOMdddaJdjcacMJa@@@@@dOJddddOO@@                                
+#                                                     @  @@@@@@dda@MdO@O@MOaa@>  @-@d@adOOadO@                                
+#                                                   > -@ a>@  @@a@ @@@-d@>aa@@ O{@ @OajOOdOddM@                               
+#                                                  d@  @  @I {  @@  @@@@@M@@d@--@  @c@JaOdOOOO@@                              
+#                                                @   @ @ M@ @j@ c  @M @@ "@ @@@ >@@Mc@cMOdOOJaO@@                             
+#                                              @    @O @O@@a @@@     @ M"   @{ j@IMacaOaOOOMaaO@@                             
+#                                           @ @   @J@@@aOM@ M@@  "   { @ @@@  M@  @@aOOOMOOaO@O@ @                            
+#                                            @  a |Mad@a@d@            @@@@-@ @@@@@M@OJJMOOMO@ @d@                            
+#                                              > @@d@O@@@O@ " O       @ @@   M@    @cJM@Jaaa@@ @ j                            
+#                                            j   @@@>@ @@@@                @@@@ "  @dMdJJd@ @ d@ @                            
+#                                             I@ @ @ @  j@@  Ij "       -@{@d@    @@dMdO@@@@  @  @                            
+#                                              {O@       @ @    I"  @@   @d@M a@@@@@@@@@d@ @ @  @                             
+#                                                @  d@    O@@@       -@@@@@|@@@@@@ @ @  @ @    @                              
+#                                                @ >  @@cM  "{@    @@@@ c|@@@O @   @  d  |   "                                
+#                                     >--@@@@@@@add@d |dd   O{dO@@@J - I          c                                           
+#                                      -   a@@@@@d@  jO>da@@Jj|@  "{ @--I-j@@@@                                               
+#                                    I jca@I       cJJj@@ I @  @       c@>I   @                                               
+#                                 Mc@@@@@"        @@a@-  |@  @           j{ |                                                 
+#                              @@@@@@@dJO      @@@@@  {@    c            J @   I                                              
+#                                      > @ad@@M@j   @     c{              O >  -@                                             
+#                                       @M@d@ d       MMJO>               c @ |                                               
+#                                     @@|   d    "--     -                c @ >{ @                                            
+#                                         M M        " " @ {" >>>>>>{cI  {I>      @                                           
+#                                       @@@  >     c         Oc>   {  d a  I J {  M"                                          
+#                                       {@ a@{|{d  |   """  Jd   c   I{O   a @|@  @ >             M@@@@M@@                    
+#                                      @@@d@ c{>>>Mc       c       " Ij {I I @    >         @@           I@@I                 
+#                                    @@@@      j@ >||cI          -    M > "  M  a "|     @@                 @@                
+#                               I >d-         @     c->{{|{|jcJd      >   """{     -  "@@                    O@               
+#                               "  >  @ @   @        c|"   ">-      "a "    @ {> aj  @@                      @                
+#                                    @    @            |@@J       c||| > j  @    j @@                        @                
+#                             @j@@@ @   @            @@"  @@@@@@@@@  "   O{@  j @cM                         @                 
+#                             @@@    >>            @I "c{{ j  -@@@M@@| @"  M   Oaa                         @                  
+#                           M@@@ |M-             @I">| -       > @@@@@@@  a" aM   c O@@@@@@@@@@@@@@|      @                   
+#                          @@@    >           Ij>|I-{      J   {    @@@@@IMa@@M@@                  @@@a@@@                    
+#                        @@@@   |           @>|j{-jI  "     c   J  j  @@@ @>@d c-  @J                  @   @@                 
+#                       @@@    @           @>O   |j   " |    j  c  O"  J@ I>  @@dac   d             J@       >@               
+#                     @@@@    c  "        J>-    cj   " { " """  a  a  j O"       @OMdM@@-       O@@           @J             
+#                    @@@     -           jj{     |j  " "   "    " >  M  {"a         aO  @@@@@@@@@                @            
+#                  @@@@      "         @|cI |     c      {  " "  | >  c   J@         @M  M                        @           
+#                 @@@       "         M|    |     Od "  J I  I     |-      J@         "@ @           |>I  >       @           
+#               @@@@     a  "       @-j     -  "   c        "        |>  j   @          @J        c        j     @d-          
+#             {@@@      @      >  >--"j      "      M    - j       "  I    j  @          @>  I"I"   "|O> Ica    @Jj           
+#            @@@@      M         JcO j       " "         d  J           {      @a    c" |{@-                > {@@             
+#          c@@@                O-{|  O  "    - "     -    >  O            "|    M@ a|-  @J O@@@    I>>>"    @cjI  -           
+#         @@@d      @ "      @J->|  O"    "" J  "     I"" O   J      -       @    dM    |@    a@Mad@@@@@@{      >{jI|d- {     
+#   @@c" @@@      j        @ |I>-> -J   O       "         >O   J      {        dJ  Mj@"dJ                 -Ja>        I>jI>>  
+#  @       {    "{  "    {  j|| "  J          c   "      > c"   O  "   |         "d| M{c  -JMJ        I|I      "    "      "  
+#             a         M  -||     d " {      -  I      " { c    a  "  " I          I>J|a@      "- "       c      "           
+#             -       @    d>  J  j  " |       O   ""       JJ   "c     " |          @  j>@@                  O>     -{ {Jc   
+#                   "     M       a     "      {       "     dJ    dI  "  ""|    "  @      I@@O           """    @"    |  >   
+# J@@j             @     M    O       I "       a  "" "        M    >d    "   -    @          @M c|           "    @d         
+# M    I@ "I"""I-">     d  "     c" " { "    "  >j         "    M     a"    "   "@     I ""     @jI c>   - "    "    {cadd|   
+#           "             "  |   >  " |    "     O        "       II    a      @c c          I    @JJ  |    "-                
+#                      |I    - J  " " {    " I "  d           ""  ""         @@     @       "       @dj  "I    { -        >   
+#                        I     "  I   - "  "  I   >|         "  "    |    @d@         @         "     @@    >    >  {     >   
+#                     -{ I  {  I-     -     " " "  dc   "        "     @aO@             dJ          "   d@   I     |  {  JI   
+#                    I-  "  {  j- "   { |      "    O{             @O@O@      J           Oa             MM    J "     OcJ    
+#                        "  "  J> " " { @    " "" "  J        @@@@a@-     >    |"           Od  I-        @@         c{  -I   
+#                   {      "   J      { J    ""       |M@M@@@O         "    J   Ic           -cJ   -"      "@  "  I @{--      
+#                  -       "   c      >         M@@@@" c       "      "  "     { "O            Occ    J      @       |{|      
+#                  "       |   a         O@@@I          O I >           "      |  -J            -jJ     j     @  "    c-      
+#                 - "      j   d     @@M O       "   "  >I - O            " "    " "O             j{-     |    @      j>Oj>   
+#                I "       -    |@@    >  j       "      j  | O"           "  ""  J  >   |         J-J     ""   @    >j>  I   
+#              jI "   M     I@@ J    "    O             " |  | cO         " ""      d  |   "{       >-c >    c  J      -{>    
+#             I  "    j "@M@    a       j IO     "  "      -  - jJ           I  I    d"  |    |" {   -{{ a    c  @            
+#           {        @cO@  "{O       " "   O>      " -   "        a"       "" "       jd   ""    > I  j|  a    -  @  "        
+#       -I    a>O @a|@>   I-I>|j -    "  O  |      "  {"           >{       "I "        O   j   "  c"  cJ  d    | @@     M    
+#      @j{j{{" >c{@ II     J|{I--|   ""   " Oj         d "      {"   j        " I        O   I       |j O  --  a"  @   Jc -   
+#       {>I  M||O"--|j       O{-{j|    " "   d-I       "@        |>"  c        "          O    I-  "   >c   O {-|  @@cdj@     
+#       O- @>a """-Ij          >Ij   " " ""|  jJ{cj|{    O -       j           "           a    jI      >   O||J    @         
+#        @O@>">>>->>|   "OjJj{O-j{   "  " " - I>>>-I|{c   | {   "   >"I ">""      " c    "  a "  {I"  "   "  {   j  @|        
+#      d@d   "j->-II{{|j->I>>"-"II{j        >j||{-j{--|{{||  -     {      >           I         "   c      Ij|jjJ   @a        
+#    "@d      -IIIIII""I"I"I-II"I>"-{-         "     >II"I>>         -     {           | I   >  " "  |       ">j    OJ        
+#   Jc         -II""""I>>"I " """"" "I{     -I      >I""I""I{>      I                   >  "         {I  |-II       jj        
+#  >                ""                  "I"" -"I""I                      "   "  >        "" "         """           -I        
+#                                                                                                                   "         
+#                                                                                                                             
+#                                                                                                                             
+# ############################################################################################################################
+
+import os
+import re
+import shutil
+from datetime import datetime
+
+IGNORE_DEACTIVATED_FILE = True
+DEBUG = False
+
+def insert_in_array(array, pos, value):
+	lst = array[0:pos] + [value] + array[pos:]
+	return lst
+
+def remove_runs_form_block(input_array):
+	for idx, runs in enumerate(input_array):
+		dprint("remove_runs_form_block " + str(idx) + ": " + runs)
+		if re.sub('[\s+]', '', runs).startswith('run'):
+			input_array.pop(idx)
+			
+	for idx, runs in enumerate(input_array):
+		dprint("remove_runs_form_block " + str(idx) + ": " + runs)
+		if re.sub('[\s+]', '', runs).startswith('run'):
+			input_array.pop(idx)
+			
+	return input_array
+
+def dprint(input):
+	global DEBUG
+	
+	if DEBUG:
+		print(input)
+
+def recalculateCommandlist(input_array, has_normalmap):
+	dprint("recalculateCommandlist(array, " + str(has_normalmap) + ")")
+	runlist = []
+	
+	others = []
+	ids_to_delete = []
+	for idx, runs in enumerate(input_array):
+		print("re " + str(idx) + ": " + runs)
+		if re.sub('[\s+]', '', runs).startswith('run'):
+			if "\\ORFix\\ORFix" not in re.sub('[\s+]', '', runs) and "\\ORFix\\NNFix" not in re.sub('[\s+]', '', runs):
+				others.append(runs)
+	
+	if has_normalmap == True:
+		runlist.append("run = CommandList\\global\\ORFix\\ORFix")
+	else:	
+		runlist.append("run = CommandList\\global\\ORFix\\NNFix")
+			
+	if len(others) != 0:
+		for run in others:
+			runlist.append(run)
+		
+	dprint(runlist)
+	return runlist
+
+def replace_ini_content(content):
+	blocks = []
+	lines = []
+	
+	token = content.split("\r\n")
+
+	if len(token) == 1:
+		token = content.splitlines()
+
+	# analyse und in blöcke packen
+	has_run = False
+	has_ps_t = False
+	has_if = False
+	has_normalmap = False
+	is_type = ''
+	last_block = 'root'
+	for line in token:
+		original_line = line = line.split('\n')[0]
+		line = re.sub('[\s+]', '', line)
+		
+		if line.startswith('[') and line.endswith(']'):
+			if is_type == '':
+				is_type = last_block.replace('[', '').replace(']', '')
+			
+			data = {}
+			data['lines'] = lines
+			data['has_run'] = has_run
+			data['has_ps_t'] = has_ps_t
+			data['has_if'] = has_if
+			data['has_normalmap'] = has_normalmap
+			data['type'] = is_type
+			blocks.append(data)
+			
+			last_block = line
+			lines = []
+			has_run = False
+			has_ps_t = False
+			has_if = False
+			has_normalmap = False
+			is_type = ''
+			
+		if line.startswith('ps-t'):
+			has_ps_t = True
+			
+			if "NormalMap" in line:
+				has_normalmap = True
+			
+		if line.startswith('run'):
+			has_run = True
+			
+		if "endif" in line:
+			has_if = True
+			
+		if line.startswith('vb'):
+			is_type = 'vb'
+		elif line.startswith('ib'):
+			is_type = 'ib'
+		elif line.startswith('type'):
+			is_type = 'resource'
+		elif line.startswith('filename'):
+			is_type = 'resource'
+		elif line.startswith('condition'):
+			is_type = 'condition'
+
+		lines.append(original_line)
+
+	# letzter Block
+	if is_type == '':
+		is_type = last_block.replace('[', '').replace(']', '')
+	
+	data = {}
+	data['lines'] = lines
+	data['has_run'] = has_run
+	data['has_ps_t'] = has_ps_t
+	data['has_if'] = has_if
+	data['has_normalmap'] = has_normalmap
+	data['type'] = is_type
+	blocks.append(data)
+
+	# bearbeiten
+	for my_block in blocks:
+		if my_block['has_ps_t']:
+			dprint('===============================')
+			dprint(my_block)
+			
+			runs = recalculateCommandlist(my_block['lines'], my_block['has_normalmap'])
+			
+			my_block['lines'] = remove_runs_form_block(my_block['lines'])
+			dprint(my_block['lines'])
+						
+			if my_block['has_run'] and my_block['has_if']:
+				dprint('case has_run and has_if')
+				last_ps_t = 0
+				last_endif = 0
+				for idx, edit_line in enumerate(my_block['lines']):
+					dprint(str(idx) + ": " + edit_line)
+					if re.sub('[\s+]', '', edit_line).startswith('ps-t'):
+						last_ps_t = idx
+					
+					if "endif" in re.sub('[\s+]', '', edit_line):
+						last_endif = idx
+						
+				if last_endif != 0:
+					dprint("case 2 last_ps_t: " + str(last_ps_t))
+					for ridx, run in enumerate(runs):
+						my_block['lines'] = insert_in_array(my_block['lines'], last_endif + ridx + 1, run)
+						
+			else:
+				dprint('case else')
+				last_ps_t = 0
+				last_endif = 0
+				for idx, edit_line in enumerate(my_block['lines']):
+					dprint(str(idx) + ": " + edit_line)
+					if re.sub('[\s+]', '', edit_line).startswith('ps-t'):
+						last_ps_t = idx
+					
+					if "endif" in re.sub('[\s+]', '', edit_line):
+						last_endif = idx
+						
+				dprint("last_ps_t: " + str(last_ps_t) + " last_endif: " + str(last_endif))
+						
+				if last_ps_t != 0 and last_endif == 0:
+					dprint("case 1 last_ps_t: " + str(last_ps_t))
+					for ridx, run in enumerate(runs):
+						my_block['lines'] = insert_in_array(my_block['lines'], last_ps_t + 1 + ridx, run)
+					
+				elif last_endif != 0:
+					dprint("case 2 last_ps_t: " + str(last_ps_t))
+					for ridx, run in enumerate(runs):
+						my_block['lines'] = insert_in_array(my_block['lines'], last_endif + 1 + ridx, run)
+						
+	# überarbeiten
+	for block in blocks:
+		if block['lines'][-1] != "":
+			block['lines'].append("")
+
+	# zusammenführen
+	new_content = ''
+	for block in blocks:
+		for new_line in block['lines']:
+			new_content = new_content + new_line + "\n"
+
+	dprint(new_content)
+	return new_content
+
+def process_directory(directory):
+	global IGNORE_DEACTIVATED_FILE
+	
+	for root, dirs, files in os.walk(directory):
+		for file in files:
+			if file.lower().endswith('.ini'):
+				file_path = os.path.join(root, file)
+				
+				print(file_path.replace(directory, '...'))
+				if IGNORE_DEACTIVATED_FILE and "DISABLED" in file_path:
+					print('Übersprungen')
+					continue
+				
+				try:
+					with open(file_path, 'r', encoding='utf-8') as f:
+						content = f.read()
+						
+						modified = False
+						new_content = content
+						
+						processed_content = replace_ini_content(content)
+						if processed_content != content:
+							new_content = processed_content
+							modified = True
+						
+					if modified:
+						new_file = file.replace('.ini', '.txt')
+						currdate = datetime.now().strftime('%Y%m%d_%H%M%S')
+						backup_path = os.path.join(root, f"DISABLED_BACKUP_{currdate}_V61{new_file}")
+						if not os.path.exists(backup_path):
+							shutil.copy2(file_path, backup_path)
+							print(f"Sicherung erstellt: {backup_path}")
+						
+						with open(file_path, 'w', encoding='utf-8') as f:
+							f.write(new_content)
+						print(f"Geändert: {file_path}")
+						
+				except Exception as e:
+					print(f"Fehler: {file_path} - {str(e)}")
+
+if __name__ == "__main__":
+	current_dir = os.getcwd()
+	print(f"Ordner: {current_dir}")
+	process_directory(current_dir)
+	print("Fertig!")
